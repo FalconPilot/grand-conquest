ceacb01e4fc1d2487dffd087c11ecb19bf31a46e
Raphael Pallies <fpilot.github@gmail.com>
2018-08-07 15:16:57 +0200

    fixed JS issues, added Flash header core

--- backend/js/main.js
+++ backend/js/main.js
@@ -3,7 +3,7 @@
 */
 
 var url = new URL(window.location.href);
-var flash = url.searchParams.get("search");
+var flash = url.searchParams.get("flash");
 if (flash) {
   window.history.replaceState(null, null, url.pathname);
 }
