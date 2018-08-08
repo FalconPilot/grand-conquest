c4f0db17881353b2925330fbf8eacf0d181e61f0
Raphael Pallies <fpilot.github@gmail.com>
2018-08-08 11:19:00 +0200

    hotfix : repaired vertical height in buttons, fixed 500 error in App viewport

--- backend/app/index.php
+++ backend/app/index.php
@@ -1,6 +1,9 @@
 <?php
 
-include(dirname(__FILE__)."/../includes.php");
+include(dirname(__FILE__)."/../init.php");
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 FpSession::initSession(function() {
   define('PAGEVIEW', './app.html');
