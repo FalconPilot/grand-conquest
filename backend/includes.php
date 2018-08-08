d124c5479ef7c28555e9473ef848e16325f2e076
FalconPilot <fpilot.github@gmail.com>
2018-08-08 02:38:42 +0200

    hotfix : backend version moved to generic location

--- backend/includes.php
+++ backend/includes.php
@@ -1,5 +1,8 @@
 <?php
 
+// GC Backend Version
+define('GC_VERSION', '0.1.3');
+
 // Include common files
 $base = dirname(__FILE__);
 include($base."/lib/FpArray.php");
--- backend/index.php
+++ backend/index.php
@@ -3,9 +3,6 @@
 // Common includes
 include('./includes.php');
 
-// GC Backend Version
-define('GC_VERSION', '0.1.3');
-
 // Check session
 session_start();
 if (!isset($_SESSION['id'])) {
